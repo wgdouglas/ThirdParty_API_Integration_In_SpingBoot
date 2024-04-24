e64ce984cc8e24db21d6d99ea6a14d0b4fe4259d
gfizzle <winstongdouglas4@gmail.com>
2024-04-23 20:04:33 -0500

    first commit

---
+++ src/main/java/com/example/thirdparty_api_integration_in_springboot/ThirdPartyApiIntegrationInSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.thirdparty_api_integration_in_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdPartyApiIntegrationInSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThirdPartyApiIntegrationInSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/thirdparty_api_integration_in_springboot/ThirdPartyApiIntegrationInSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thirdparty_api_integration_in_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirdPartyApiIntegrationInSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
